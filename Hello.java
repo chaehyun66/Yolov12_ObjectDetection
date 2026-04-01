a0d2a34079b8a4cd9aabd26158c18fee81657e80
chaehyun66 <152498268+chaehyun66@users.noreply.github.com>
2026-04-01 10:53:53 +0900

    samplefile

---
+++ Hello.java
@@ -0,0 +1,9 @@
+// src/Hello.java
+
+public class Hello {
+    public static void main(String[] args) {
+        // 화면에 환영 인사를 출력합니다.
+        System.out.println("Hello, Java World!");
+    }
+    
+}
